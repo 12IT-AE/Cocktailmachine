852201feef5a5d926cf1357cb56068fc226e7969
TheonlyTazz <TheonlyTazz@gmail.com>
2024-11-08 10:42:20 +0100

    header only if admin

--- Frontend/resources/views/layouts/app.blade.php
+++ Frontend/resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
 <body>
     <div class="container mt-5">
+        
+        @if(session('admin'))
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">Drinkpad '24</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -44,6 +46,8 @@
                 </ul>
             </div>
         </nav>
+        @endif
+
         @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
