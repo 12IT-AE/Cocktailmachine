1306888169cddb440d45adabb966ac967da33c3a
0chse <53704790+0chse@users.noreply.github.com>
2024-11-22 16:48:46 +0100

    Popup Update

--- Frontend/resources/views/recipe/modal.blade.php
+++ Frontend/resources/views/recipe/modal.blade.php
@@ -1,12 +1,12 @@
 
 
 <div class="modal fade" id="modal-xl" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-fullscreen" style="width: 90%; margin: 0 auto">
-        <div class="modal-content">
+    <div class="modal-dialog modal-fullscreen" style="width: 90%; margin: 0 auto;">
+        <div class="modal-content" style='background-color: transparent; backdrop-filter: blur(6px);' >
 
 
 <div class="container mt-5">
-    <div class="card shadow-sm">
+    <div class="order-card shadow-sm">
         <div class="card-body">
             <div class="row">
                 <div class="col-md-4">
