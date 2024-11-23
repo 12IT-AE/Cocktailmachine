6760cbc641a727e1d3fef04a0aec9f7f646d1464
lenny <lennye.bremmer@gmail.com>
2024-11-23 01:02:15 +0100

    pumpcontrol optimiert

--- Backend/pumpcontrol.py
+++ Backend/pumpcontrol.py
@@ -5,35 +5,43 @@ try:
     import RPi.GPIO as GPIO
 except:
     import Mock.GPIO as GPIO
- 
 
+# Initialisiere GPIO Pin
+def setup_gpio(pin, state):
+    if pin is not None:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, state)
+    else:
+        logging.error(f"GPIO {pin} ist ungültig!")
+
+# Startet die Pumpe
 def start_pump(gpio_pin):
     print(f"Start Pump: {gpio_pin}")
     if gpio_pin is not None:
-        GPIO.setmode(GPIO.BCM)
         logging.info(f"GPIO {gpio_pin}: starting")
-        GPIO.setup(gpio_pin, GPIO.OUT)
-        GPIO.output(gpio_pin, GPIO.LOW)
+        setup_gpio(gpio_pin, GPIO.LOW)
     else:
         logging.error(f"GPIO {gpio_pin} nicht gefunden!")
 
+# Stoppt die Pumpe
 def stop_pump(gpio_pin):
     print(f"Stop Pump: {gpio_pin}")
     if gpio_pin is not None:
-        GPIO.setmode(GPIO.BCM)
         logging.info(f"GPIO {gpio_pin}: stopping!")
-        GPIO.setup(gpio_pin, GPIO.OUT)
-        GPIO.output(gpio_pin, GPIO.HIGH)
+        setup_gpio(gpio_pin, GPIO.HIGH)
     else:
         logging.error(f"GPIO {gpio_pin} nicht gefunden!")
 
 def cleanPumps(sec):
     all_pumps = Pump.Database().selectAllFromDatabase()
+    if not all_pumps:
+        logging.warning("Keine Pumpen in der Datenbank gefunden!")
+        return
+    print("Starte Reinigung aller Pumpen...")
     for pump in all_pumps:
         if pump.pin is not None:
-            print(f"GPIO {pump.pin}: cleaning")
-            GPIO.setup(pump.pin, GPIO.OUT)
-            GPIO.output(pump.pin, GPIO.LOW)
+            print(f"Reinige Pumpe GPIO {pump.pin}")
+            setup_gpio(pump.pin, GPIO.LOW)
         else:
             logging.error(f"GPIO {pump.pin} nicht gefunden!")
 
@@ -41,13 +49,15 @@ def cleanPumps(sec):
 
     for pump in all_pumps:
         if pump.pin is not None:
-            GPIO.setup(pump.pin, GPIO.OUT)
-            GPIO.output(pump.pin, GPIO.HIGH)
-        else:
-            logging.error(f"GPIO {pump.pin} nicht gefunden!")
-    print(f"fertig")
+            setup_gpio(pump.pin, GPIO.HIGH)
+    print(f"Reinigung abgeschlossen!")
 
+# Startet eine Pumpe für eine bestimmte Dauer
 def start_pumpfor(gpio_pin, sec):
+    if gpio_pin is None:
+        print("Ungültiger GPIO-Pin! Vorgang abgebrochen.")
+        return
+    GPIO.setmode(GPIO.BCM)
     start_pump(gpio_pin)
     time.sleep(sec)
     stop_pump(gpio_pin)
